c898ff1c1dbdb8efb98005da5f73d5d7627fbe00
kawamataryo <ba068082@gmail.com>
2022-04-03 12:00:37 +0900

    fix: timeout setting

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,9 +5,15 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 
 const sleep = (ms: number): Promise<void> => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+};
+
+const executeCommandWithWait = async (command: string): Promise<any> => {
+	await sleep(500);
+	await vscode.commands.executeCommand(COMMAND_NAME);
+	await sleep(1000);
 };
 
 const COMMAND_NAME = 'copy-python-path.copy-python-path';
@@ -15,16 +21,16 @@ const COMMAND_NAME = 'copy-python-path.copy-python-path';
 const testFileLocation = '/pythonApp/example.py';
 /* test file is following code
 class ClassA:
-    def class_a_method_a():
-        pass
+		def class_a_method_a():
+				pass
 
-    class ClassB:
-       def class_b_method_a():
-           pass
+		class ClassB:
+			 def class_b_method_a():
+					 pass
 
 class ClassD:
-    def class_d_method_a():
-       pass
+		def class_d_method_a():
+			 pass
 */
 
 suite('Extension Test Suite', () => {
@@ -41,8 +47,7 @@ suite('Extension Test Suite', () => {
 	test('selected class lines', async () => {
 		editor.selection = new vscode.Selection(new vscode.Position(0, 0), new vscode.Position(0, 0));
 
-		vscode.commands.executeCommand(COMMAND_NAME);
-		await sleep(1000);
+		await executeCommandWithWait(COMMAND_NAME);
 
 		assert.strictEqual(await vscode.env.clipboard.readText(), 'pythonApp.example.ClassA');
 	});
@@ -50,8 +55,7 @@ suite('Extension Test Suite', () => {
 	test('selected method lines', async () => {
 		editor.selection = new vscode.Selection(new vscode.Position(1, 0), new vscode.Position(1, 0));
 
-		vscode.commands.executeCommand(COMMAND_NAME);
-		await sleep(1000);
+		await executeCommandWithWait(COMMAND_NAME);
 
 		assert.strictEqual(await vscode.env.clipboard.readText(), 'pythonApp.example.ClassA.class_a_method_a');
 	});
@@ -59,8 +63,7 @@ suite('Extension Test Suite', () => {
 	test('selected nested class lines', async () => {
 		editor.selection = new vscode.Selection(new vscode.Position(4, 0), new vscode.Position(4, 0));
 
-		vscode.commands.executeCommand(COMMAND_NAME);
-		await sleep(1000);
+		await executeCommandWithWait(COMMAND_NAME);
 
 		assert.strictEqual(await vscode.env.clipboard.readText(), 'pythonApp.example.ClassA.ClassB');
 	});
@@ -68,8 +71,7 @@ suite('Extension Test Suite', () => {
 	test('selected nested method lines', async () => {
 		editor.selection = new vscode.Selection(new vscode.Position(5, 0), new vscode.Position(5, 0));
 
-		vscode.commands.executeCommand(COMMAND_NAME);
-		await sleep(1000);
+		await executeCommandWithWait(COMMAND_NAME);
 
 		assert.strictEqual(await vscode.env.clipboard.readText(), 'pythonApp.example.ClassA.ClassB.class_b_method_a');
 	});
@@ -77,8 +79,7 @@ suite('Extension Test Suite', () => {
 	test('selected other class lines', async () => {
 		editor.selection = new vscode.Selection(new vscode.Position(9, 0), new vscode.Position(9, 0));
 
-		vscode.commands.executeCommand(COMMAND_NAME);
-		await sleep(1000);
+		await executeCommandWithWait(COMMAND_NAME);
 
 		assert.strictEqual(await vscode.env.clipboard.readText(), 'pythonApp.example.ClassD');
 	});
@@ -86,8 +87,7 @@ suite('Extension Test Suite', () => {
 	test('selected lines other than symbol', async () => {
 		editor.selection = new vscode.Selection(new vscode.Position(12, 0), new vscode.Position(12, 0));
 
-		vscode.commands.executeCommand(COMMAND_NAME);
-		await sleep(1000);
+		await executeCommandWithWait(COMMAND_NAME);
 
 		assert.strictEqual(await vscode.env.clipboard.readText(), 'pythonApp.example');
 	});
